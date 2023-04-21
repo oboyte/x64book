0c1bce7eb7e0f862a24bc979b7986252ddb7b680
thomas0305 <thomashjohnsen0305@gmail.com>
2023-04-21 09:01:52 +0200

    chapt 13

---
+++ 13_stackalignment_and_stackframe/alignment.asm
@@ -0,0 +1,36 @@
+extern printf
+section .data
+    fmt     db      "2 times pi equals %.12f",10,0
+    pi      dq      3.14159265358979
+section .bss
+section .text
+;-----------------------------------------------------------------
+func3:
+    push rbp
+        movsd   xmm0,[pi]
+        addsd   xmm0, [pi]
+        mov     rdi, fmt
+        mov     rax, 1
+        call    printf
+    pop rbp
+    ret
+;-----------------------------------------------------------------
+func2:
+    push rbp
+        call func3 ; call the third function
+    pop rbp
+    ret
+;-----------------------------------------------------------------
+func1:
+    push rbp
+        call func2 ; call the second function
+    pop rbp
+    ret
+;-----------------------------------------------------------------
+    global main
+main:
+    mov rbp, rsp; for correct debugging
+    push    rbp
+        call func1
+    pop rbp
+ret
